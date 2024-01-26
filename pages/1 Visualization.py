4d354d0641663d5ab381f29717f308e38ab64f85
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2024-01-26 15:22:58 +0700

    Update 1 Visualization.py

--- pages/1 Visualization.py
+++ pages/1 Visualization.py
@@ -2,9 +2,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from termcolor import colored
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
 
-
-df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
+df = pd.read_excel('heart.xlsx')
 
 html_0 = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
@@ -23,14 +26,118 @@ html_1 = """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
 
-grouped_data = df.groupby(['Sleep Disorder', 'Gender']).size().reset_index(name='count')
-pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Gender', fill_value=0)
-fig, ax = plt.subplots(figsize=(15, 7))
-colors = ['#3085C3', '#FF9EAA', '#5CD2E6']
-pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
-plt.axis('equal')
-st.pyplot(fig)
+fig = plt.figure(figsize=(18,15))
+gs = fig.add_gridspec(3,3)
+gs.update(wspace=0.5, hspace=0.25)
+ax0 = fig.add_subplot(gs[0,0])
+ax1 = fig.add_subplot(gs[0,1])
+ax2 = fig.add_subplot(gs[0,2])
+ax3 = fig.add_subplot(gs[1,0])
+ax4 = fig.add_subplot(gs[1,1])
+ax5 = fig.add_subplot(gs[1,2])
+ax6 = fig.add_subplot(gs[2,0])
+ax7 = fig.add_subplot(gs[2,1])
+ax8 = fig.add_subplot(gs[2,2])
 
+background_color = "#ffe6e6"
+color_palette = ["#800000","#8000ff","#6aac90","#5833ff","#da8829"]
+fig.patch.set_facecolor(background_color) 
+ax0.set_facecolor(background_color) 
+ax1.set_facecolor(background_color) 
+ax2.set_facecolor(background_color) 
+ax3.set_facecolor(background_color) 
+ax4.set_facecolor(background_color) 
+ax5.set_facecolor(background_color) 
+ax6.set_facecolor(background_color) 
+ax7.set_facecolor(background_color) 
+ax8.set_facecolor(background_color) 
+
+# Title of the plot
+ax0.spines["bottom"].set_visible(False)
+ax0.spines["left"].set_visible(False)
+ax0.spines["top"].set_visible(False)
+ax0.spines["right"].set_visible(False)
+ax0.tick_params(left=False, bottom=False)
+ax0.set_xticklabels([])
+ax0.set_yticklabels([])
+ax0.text(0.5,0.5,
+         'Count plot for various\n categorical features\n_________________',
+         horizontalalignment='center',
+         verticalalignment='center',
+         fontsize=18, fontweight='bold',
+         fontfamily='serif',
+         color="#000000")
+
+# Sex count
+ax1.text(0.3, 160, 'Sex', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax1.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax1,data=df,x='sex',palette=color_palette)
+ax1.set_xlabel("")
+ax1.set_ylabel("")
+ax1.set_xticklabels(["Female(0)","Male(1)"])
+
+# Exng count
+ax2.text(0.3, 160, 'Exng', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax2.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax2,data=df,x='exng',palette=color_palette)
+ax2.set_xlabel("")
+ax2.set_ylabel("")
+ax2.set_xticklabels(["No(0)","Yes(1)"])
+
+# Caa count
+ax3.text(1.5, 120, 'Caa', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax3.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax3,data=df,x='caa',palette=color_palette)
+ax3.set_xlabel("")
+ax3.set_ylabel("")
+
+# Cp count
+ax4.text(1.5, 120, 'Cp', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax4.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax4,data=df,x='cp',palette=color_palette)
+ax4.set_xlabel("")
+ax4.set_ylabel("")
+ax4.set_xticklabels(["Typical angina(1)","Atypical angina(2)","nopain(3)","asymptomatic(4)"], rotation=15)
+
+# Fbs count
+ax5.text(0.5, 200, 'Fbs', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax5.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax5,data=df,x='fbs',palette=color_palette)
+ax5.set_xlabel("")
+ax5.set_ylabel("")
+ax5.set_xticklabels(["False(0)","True(1)"])
+
+# Restecg count
+ax6.text(0.75, 120, 'Restecg', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax6.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax6,data=df,x='restecg',palette=color_palette)
+ax6.set_xlabel("")
+ax6.set_ylabel("")
+ax6.set_xticklabels(["normal(0)","ST-T abnormality (1)","LV hypertrophy(2)"], rotation=15)
+
+# Slp count
+ax7.text(0.85, 120, 'Slp', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax7.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax7,data=df,x='slp',palette=color_palette)
+ax7.set_xlabel("")
+ax7.set_ylabel("")
+
+# Thall count
+ax8.text(1.2, 120, 'Thall', fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+ax8.grid(color='#000000', linestyle=':', axis='y', zorder=0,  dashes=(1,5))
+sns.countplot(ax=ax8,data=df,x='thall',palette=color_palette)
+ax8.set_xlabel("")
+ax8.set_ylabel("")
+
+for s in ["top","right","left"]:
+    ax1.spines[s].set_visible(False)
+    ax2.spines[s].set_visible(False)
+    ax3.spines[s].set_visible(False)
+    ax4.spines[s].set_visible(False)
+    ax5.spines[s].set_visible(False)
+    ax6.spines[s].set_visible(False)
+    ax7.spines[s].set_visible(False)
+    ax8.spines[s].set_visible(False)
 
 html_2 = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
